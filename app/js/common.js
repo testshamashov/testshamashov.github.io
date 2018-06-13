5c36a6db219c12760eab6cc1e1049b7881e24308
Alex Shamashov <alex.shamashov@gmail.com>
2018-06-13 22:02:35 +0200

    add db

---
+++ app/js/common.js
@@ -0,0 +1,30 @@
+$(function() {
+
+
+	$(".nav-item").on("click", function(){
+		$(".nav-item").removeClass("active");
+		$(this).addClass("active");
+	});
+
+	// swiper for bootstrap carousel
+	$(".carousel").swipe( {
+		swipeLeft: function() {
+			$(this).carousel("next");
+		},
+		swipeRight: function() {
+			$(this).carousel("prev");
+		},
+		allowPageScroll: "vertical"
+	});
+
+	if( $(window).width() > 1023) {
+		$('.btn-search').on('click', function(event) {
+			event.preventDefault();
+			$('.js-search-show').toggleClass('active');
+			setTimeout(function() {
+				$('.js-search-show').toggleClass('w-100');
+			}, 500);
+		})
+	}
+
+});
